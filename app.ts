95475267c1fc14af5bbb20fa2f0210c6698c062c
khanwali225 <khanwali225@yahoo.com>
2022-11-26 13:34:03 +0500

    project calculator

---
+++ app.ts
@@ -0,0 +1,30 @@
+import inquirer from "inquirer";
+let question = [{
+    name: "operator",
+    type: "list",
+    message: "what operation do you want to do?",
+    choices: ["+", "-", "/", "*"]
+},{
+    name: "first",
+    type: "number",
+    message: "plz enter first number",
+},{
+    name: "second",
+    type: "number",
+    message: "plz enter second number"
+}]
+let answer = await inquirer.prompt(question)
+//console.log(answer.first + answer.operator + answer.second)
+
+const userOperator = answer.operator;
+const num1 = answer.first;
+const num2 = answer.second;
+if (userOperator === "+") {
+    console.log(num1 + num2)
+} else if (userOperator === "-") {
+    console.log(num1 - num2)
+} else if (userOperator === "*") {
+    console.log(num1 * num2) 
+} else if (userOperator === "/") {
+    console.log(num1 / num2)
+}
